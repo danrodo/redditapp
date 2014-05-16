4b81851aa4ac5d52309ef9bab6cccc3de0c8d5b2
Daniel Rodosky <djrodosky@gmail.com>
2014-05-16 10:50:50 -0600

    initial commit
    int commit

---
+++ redditappTests/redditappTests.m
@@ -0,0 +1,34 @@
+//
+//  redditappTests.m
+//  redditappTests
+//
+//  Created by Daniel Rodosky on 5/16/14.
+//  Copyright (c) 2014 Dan Rodosky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface redditappTests : XCTestCase
+
+@end
+
+@implementation redditappTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
